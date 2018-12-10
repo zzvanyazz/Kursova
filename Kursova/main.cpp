e9e49d832c43be212b63de0a8e7ef3a794a02679
LAPTOP-01VRFI8J\Іван Бекер <4141zx@gmail.com>
2018-12-10 17:28:13 +0200

    no message

--- Kursova/main.cpp
+++ Kursova/main.cpp
@@ -16,7 +16,6 @@ int main(int argc, char *argv[])
         while (!stack.isEmpty())
 
     qDebug() << stack.pop() << endl;
-        qDebug() <<  "test 3 " << endl;
 
 
         EducationProgressMainWindow
